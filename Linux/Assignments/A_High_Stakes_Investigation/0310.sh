593c3e690e64db424c1dc7c9b8ca4c93370f3d3a
Everton <105988813+evertonwilliamsjr@users.noreply.github.com>
2023-04-07 13:47:19 -0400

    Update 0310.sh

--- Linux/Assignments/A_High_Stakes_Investigation/0310.sh
+++ Linux/Assignments/A_High_Stakes_Investigation/0310.sh
@@ -4,6 +4,7 @@
 cd /03-student/Lucky_Duck_Investigation/Roulette_Loss_Investigation/Dealer_Analysis
 
 head -n1 0310_Dealer_schedule | awk '{print $1, $2, $5, $6}'
+echo
 grep '05:00:00 AM' 0310_Dealer_schedule | awk '{print $1, $2, $5, $6}'
 grep '08:00:00 AM' 0310_Dealer_schedule | awk '{print $1, $2, $5, $6}'
 grep '02:00:00 PM' 0310_Dealer_schedule | awk '{print $1, $2, $5, $6}'
